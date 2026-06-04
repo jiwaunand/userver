6c5a88f3144cd5b5a9875deebce7e63b133f1859
JiwaArch <2511512002_muhammad@unand.ac.id>
2026-06-04 23:35:01 +0700

    test 1

--- script/nginx-html-update.sh
+++ script/nginx-html-update.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-cp $HOME/userver/src/nginx-httpd
+PROJECTNAME="nginx-httpd"
+
+[ ! -d "/var/www/$PROJECTNAME" ] && mkdir -p "/var/www/$PROJECTNAME"
+cp "/home/vbox/userver/src/$PROJECTNAME/index.html" "/var/www/$PROJECTNAME/index.html"
+
+rm /etc/nginx/sites-enabled/neotelemetrimodul4
+cp "/home/vbox/userver/src/$PROJECTNAME/neotelemetrimodul4" /etc/nginx/sites-available/neotelemetrimodul4
+ln -s /etc/nginx/sites-available/neotelemetrimodul4 /etc/nginx/sites-enabled/
+
+nginx -t
+systemctl reload nginx
